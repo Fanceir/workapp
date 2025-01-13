870899788e3b4cf9be329e1b264375c3fc20cb51
fanceir <fx_official@outlook.com>
2025-01-13 15:53:20 +0800

    fix: delete reset page

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { SignInFlow } from "@/features/auth/types";
 import { SignInCard } from "@/features/auth/components/sign-in-card";
 import { SignUpCard } from "@/features/auth/components/sign-up-card";
-import { ResetPassword } from "@/features/auth/components/reset-password";
 export const AuthScreen = () => {
   const [state, setState] = useState<SignInFlow>("signIn");
   return (
@@ -12,7 +11,6 @@ export const AuthScreen = () => {
       <div className="md:h-auto md:w-[420px]">
         {state === "signIn" && <SignInCard setState={setState} />}
         {state === "signUp" && <SignUpCard setState={setState} />}
-        {state === "resetPassword" && <ResetPassword  setState={setState}/>}
       </div>
     </div>
   );
