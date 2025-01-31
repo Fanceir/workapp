4c2aa2ce6c020959716e03d37f319f885ce1b68d
fanceir <fx_official@outlook.com>
2025-01-31 22:15:55 +0800

    feat: 图片上传功能

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,15 +1,24 @@
 import { useCreateMessage } from "@/features/messages/api/use-create-message";
+import { useGenerateUploadUrl } from "@/features/upload/api/use-generate-upload-url";
 import { useChannelId } from "@/hooks/use-channel-id";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import dynamic from "next/dynamic";
 import Quill from "quill";
 import { useRef, useState } from "react";
 import { toast } from "sonner";
+import { Id } from "../../../../../../convex/_generated/dataModel";
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
 interface ChatInputProps {
   placeholder?: string;
 }
+
+type createMessageValues = {
+  channelId: Id<"channels">;
+  workspaceId: Id<"workspaces">;
+  body: string;
+  image: Id<"_storage"> | undefined;
+};
 
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
   const [editorKey, setEditorKey] = useState(0);
@@ -18,6 +27,7 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
   const channelId = useChannelId();
   const workspaceId = useWorkspaceId();
   const { mutate: createMessage } = useCreateMessage();
+  const { mutate: generateUploadUrl } = useGenerateUploadUrl();
   const handleSubmit = async ({
     body,
     image,
@@ -27,21 +37,37 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
   }) => {
     try {
       setIsPending(true);
-
-      await createMessage(
-        {
-          workspaceId,
-          channelId,
-          body,
-        },
-        { throwError: true }
-      );
+      editorRef?.current?.enable(false);
+      const values: createMessageValues = {
+        channelId,
+        workspaceId,
+        body,
+        image: undefined,
+      };
+      if (image) {
+        const url = await generateUploadUrl({}, { throwError: true });
+        if (!url) {
+          throw new Error("上传失败");
+        }
+        const result = await fetch(url, {
+          method: "POST",
+          headers: { "Content-Type": image.type },
+          body: image,
+        });
+        if (!result.ok) {
+          throw new Error("上传失败");
+        }
+        const { storageId } = await result.json();
+        values.image = storageId;
+      }
+      await createMessage(values, { throwError: true });
       // editorRef?.current?.setContents([]);
       setEditorKey((prev) => prev + 1);
-    } catch (e) {
+    } catch {
       toast.error("发送失败");
     } finally {
       setIsPending(false);
+      editorRef?.current?.enable(true);
     }
   };
   return (
@@ -51,7 +77,7 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
         key={editorKey}
         placeholder={placeholder}
         onSubmit={handleSubmit}
-        disabled={false}
+        disabled={pending}
         innerRef={editorRef}
       />
     </div>
