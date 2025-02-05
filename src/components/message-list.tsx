51ab64d8cf77354ffdf1424149dc5234243f3674
fanceir <fx_official@outlook.com>
2025-02-05 11:31:10 +0800

    feat: 频道标题完成

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -3,7 +3,8 @@ import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages"
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { differenceInMinutes, format, isToday, isYesterday } from "date-fns";
 import { Message } from "./message";
-
+import { ChannelHero } from "./channel-hero";
+import { useState } from "react";
 const TIME_THRESHOLD = 1;
 interface MessageListProps {
   memberName?: string;
@@ -38,8 +39,12 @@ export const MessageList = ({
   isLoadingMore,
   canLoadMore,
 }: MessageListProps) => {
+  const [editingId, setEditingId] = useState<Id<"messages"> | null>(null);
+
   const workspaceId = useWorkspaceId();
+
   const { data: currentMember } = useCurrentMember({ workspaceId });
+
   const groupedMessages = data?.reduce(
     (groups, message) => {
       const data = new Date(message._creationTime);
@@ -53,8 +58,8 @@ export const MessageList = ({
     {} as Record<string, typeof data>
   );
   return (
-    <div className="flex-1 flex flex-col-reverse pb-4 overflow-y-auto messages-scrollbar">
-      {Object.entries(groupedMessages || {}).map(([dateKey, messages]) => (
+    <div className="flex flex-1 flex-col-reverse pb-4 overflow-y-auto messages-scrollbar">
+      {Object.entries(groupedMessages || {})?.map(([dateKey, messages]) => (
         <div key={dateKey}>
           <div className="text-center my-2 relative">
             <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
@@ -78,24 +83,55 @@ export const MessageList = ({
                 memberId={message.memberId}
                 authorImage={message.user.image}
                 authorName={message.user.name}
-                isAuthor={currentMember?._id === message.memberId}
+                isAuthor={message.memberId === currentMember?._id}
                 reactions={message.reactions}
                 body={message.body}
                 image={message.image}
+                updatedAt={message.updatedAt}
+                isEditing={editingId === message._id}
+                setEditingId={setEditingId}
+                isCompact={isCompact}
+                hideThreadButton={variant === "thread"}
                 createdAt={message._creationTime}
-                updatedAt={message._creationTime}
-                hideThreadButton={false}
                 threadCount={message.threadCount}
                 threadImage={message.threadImage}
+                threadName={message.threadName}
                 threadTimestamp={message.threadTimestamp}
-                isEditing={false}
-                isCompact={isCompact}
-                setEditingId={() => {}}
               />
             );
           })}
         </div>
       ))}
+      <div
+        className="h-1"
+        ref={(el) => {
+          if (el) {
+            const observer = new IntersectionObserver(
+              ([entry]) => {
+                if (entry.isIntersecting && canLoadMore) {
+                  loadMore();
+                }
+              },
+              { threshold: 1.0 }
+            );
+
+            observer.observe(el);
+
+            return () => observer.disconnect();
+          }
+        }}
+      />
+      {isLoadingMore && (
+        <div className="text-center my-2 relative">
+          <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+          <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm">
+            <Loader className="size-4 animate-spin" />
+          </span>
+        </div>
+      )}
+      {variant === "channel" && channelName && channelCreationTime && (
+        <ChannelHero name={channelName} creationTime={channelCreationTime} />
+      )}
     </div>
   );
 };
